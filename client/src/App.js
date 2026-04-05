674f4e8721ffc5a3b11886b1ac0201ec988512bd
Raj Rathore <rajrathore2902@gmail.com>
2026-04-05 20:05:30 +0530

    feat: setup react project structure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>TaskFlow</h1>
+      <p>Your Kanban Board is coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
